4b44d7eafba1c27b635c94a94d9ae48fd1ebfb1d
rsravankumar1109 <rsravan@aapthitech.com>
2024-01-30 11:07:50 +0530

    template
    template

--- components/templateForm.tsx
+++ components/templateForm.tsx
@@ -281,7 +281,6 @@ const TemplateForm = (props: { id: number }) => {
                     id="app_id"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     defaultValue={app_id}
-                 
                   >
                     <option key={0} value="">
                       {"Select"}
